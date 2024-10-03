fbc1c9c0c06e10ec639b01d37448f0f17dfa81ba
Jonathon Broughton <760691+jsdbroughton@users.noreply.github.com>
2024-10-03 15:38:04 +0100

    Jbr (#6)
    * trimmed scaffold
    
    * commit

--- main.py
+++ main.py
@@ -5,36 +5,17 @@ Use the automation_context module to wrap your function in an Automate context h
 
 from pydantic import Field, SecretStr
 from speckle_automate import (
-    AutomateBase,
     AutomationContext,
     execute_automate_function,
 )
 
 from flatten import flatten_base
 
-
-class FunctionInputs(AutomateBase):
-    """These are function author-defined values.
-
-    Automate will make sure to supply them matching the types specified here.
-    Please use the pydantic model schema to define your inputs:
-    https://docs.pydantic.dev/latest/usage/models/
-    """
-
-    # An example of how to use secret values.
-    whisper_message: SecretStr = Field(title="This is a secret message")
-    forbidden_speckle_type: str = Field(
-        title="Forbidden speckle type",
-        description=(
-            "If a object has the following speckle_type,"
-            " it will be marked with an error."
-        ),
-    )
+from src import function
 
 
-def automate_function(
-    automate_context: AutomationContext,
-    function_inputs: FunctionInputs,
+def automate_function_without_inputs(
+    automate_context: AutomationContext
 ) -> None:
     """This is an example Speckle Automate function.
 
@@ -48,48 +29,13 @@ def automate_function(
     # The context provides a convenient way to receive the triggering version.
     version_root_object = automate_context.receive_version()
 
-    objects_with_forbidden_speckle_type = [
-        b
-        for b in flatten_base(version_root_object)
-        if b.speckle_type == function_inputs.forbidden_speckle_type
-    ]
-    count = len(objects_with_forbidden_speckle_type)
+    function.bjorn_magic(version_root_object)
 
-    if count > 0:
-        # This is how a run is marked with a failure cause.
-        automate_context.attach_error_to_objects(
-            category="Forbidden speckle_type"
-            f" ({function_inputs.forbidden_speckle_type})",
-            object_ids=[o.id for o in objects_with_forbidden_speckle_type if o.id],
-            message="This project should not contain the type: "
-            f"{function_inputs.forbidden_speckle_type}",
-        )
-        automate_context.mark_run_failed(
-            "Automation failed: "
-            f"Found {count} object that have one of the forbidden speckle types: "
-            f"{function_inputs.forbidden_speckle_type}"
-        )
-
-        # Set the automation context view to the original model/version view
-        # to show the offending objects.
-        automate_context.set_context_view()
-
-    else:
-        automate_context.mark_run_success("No forbidden types found.")
+    automate_context.mark_run_success("No forbidden types found.")
 
     # If the function generates file results, this is how it can be
     # attached to the Speckle project/model
     # automate_context.store_file_result("./report.pdf")
-
-
-def automate_function_without_inputs(automate_context: AutomationContext) -> None:
-    """A function example without inputs.
-
-    If your function does not need any input variables,
-     besides what the automation context provides,
-     the inputs argument can be omitted.
-    """
-    pass
 
 
 # make sure to call the function with the executor
@@ -97,7 +43,4 @@ if __name__ == "__main__":
     # NOTE: always pass in the automate function by its reference; do not invoke it!
 
     # Pass in the function reference with the inputs schema to the executor.
-    execute_automate_function(automate_function, FunctionInputs)
-
-    # If the function has no arguments, the executor can handle it like so
-    # execute_automate_function(automate_function_without_inputs)
+    execute_automate_function(automate_function_without_inputs)
